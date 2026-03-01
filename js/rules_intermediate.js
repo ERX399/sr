91de5c0f6775113b512f5229f5d4af4d4af5345d
咡如夏 <e.r.x399@gmail.com>
2026-03-01 21:18:24 +0800

    Update rules_intermediate.js

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1,6 +1,6 @@
 window.RULES_INTERMEDIATE = {
     "/qc999": {
         "url": "https://123yun.com",
-        "expired_at": "2026-03-01T16:53:00.000Z"
+        "expired_at": "2026-03-01T16:53:00.000+08:00"
     }
 };
