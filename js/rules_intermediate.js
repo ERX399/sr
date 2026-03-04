9186607ff9300a86b45ec73fd68e2dcf5abaf40a
咡如夏 <e.r.x399@gmail.com>
2026-03-05 06:44:06 +0800

    添加短链: video

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2,5 +2,9 @@ window.RULES_INTERMEDIATE = {
     "/love": {
         "url": "https://hago.520pro.top/520",
         "expired_at": "2026-03-12T14:00:00+08:00"
+    },
+    "/video": {
+        "url": "http://183.66.27.19:18080/live/hagotv.flv",
+        "expired_at": "2026-03-12T06:43:00+08:00"
     }
 };
