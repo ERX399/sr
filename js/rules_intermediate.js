baf8146e69a5904cc0da4b6d00e2d7203f2a140b
咡如夏 <e.r.x399@gmail.com>
2026-03-04 14:18:13 +0800

    添加短链: love

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1,1 +1,6 @@
-window.RULES_INTERMEDIATE = {};
+window.RULES_INTERMEDIATE = {
+    "/love": {
+        "url": "https://hago.520pro.top/520",
+        "expired_at": "2026-03-12T14:00:00+08:00"
+    }
+};
