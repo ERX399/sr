02562cb9f7f762e22a91d2d0b0aa1b1299e8325e
咡如夏 <e.r.x399@gmail.com>
2026-03-07 23:36:59 +0800

    添加短链: Thinkx

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -6,5 +6,9 @@ window.RULES_INTERMEDIATE = {
     "/video": {
         "url": "http://183.66.27.19:18080/live/hagotv.flv",
         "expired_at": "2026-03-12T06:43:00+08:00"
+    },
+    "/Thinkx": {
+        "url": "https://source-ui.520pro.top/?u_atoken=69abe94f-d689-1648-cd3c-489faa3fec53&u_asig=a3b552cf17728740633834505e",
+        "expired_at": "2026-03-08T23:35:00+08:00"
     }
 };
