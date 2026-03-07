b9680199589d9e1a61d41145c4e1fae4768c158f
咡如夏 <e.r.x399@gmail.com>
2026-03-07 23:43:40 +0800

    Update rules_intermediate.js

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -3,12 +3,8 @@ window.RULES_INTERMEDIATE = {
         "url": "https://hago.520pro.top/520",
         "expired_at": "2026-03-12T14:00:00+08:00"
     },
-    "/video": {
-        "url": "http://183.66.27.19:18080/live/hagotv.flv",
-        "expired_at": "2026-03-12T06:43:00+08:00"
-    },
     "/Thinkx": {
-        "url": "https://source-ui.520pro.top/?u_atoken=69abe94f-d689-1648-cd3c-489faa3fec53&u_asig=a3b552cf17728740633834505e",
+        "url": "https://source-ui.520pro.top/",
         "expired_at": "2026-03-08T23:35:00+08:00"
     }
 };
